93e1df2337426ca4bbd0692a80076c8d4c151bc9
Rohit <rohit.lad@airbus.com>
2019-09-13 15:55:32 +0200

    A good development

--- helper.py
+++ helper.py
@@ -12,19 +12,21 @@ def reduce_memory_usage(df: pd.DataFrame, verbose=True,categories=[]):
     start_memory = df.memory_usage().sum()
     for column in df.columns:
         if column in categories:
-            df[column] = df[column].fillna('Unavailable').astype('category')
+            df[column] = assign_category(df[column])
         else:
-            col_type = df[column].dtypes
-            if col_type in data_types:
-                c_min = df[column].min()
-                c_max = df[column].max()
-                if str(col_type)[:3] == 'int':
-                    df[column] = reassign_int(c_min, c_max, df[column])
-                else:
-                    df[column] = reassign_float(c_min, c_max, df[column])
+            col_type = str(df[column].dtypes)[:3]
+            c_min = df[column].min()
+            c_max = df[column].max()
+            if col_type == 'int':
+                df[column] = reassign_int(c_min, c_max, df[column])
+            elif col_type == 'flo':
+                df[column] = reassign_float(c_min, c_max, df[column])
     end_memory = df.memory_usage().sum()
     if verbose: print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_memory/1024**2, 100 * (start_memory - end_memory) / start_memory))
     return df
+
+def assign_category(df):
+    return df.fillna('Unavailable').astype('category')
 
 def reassign_int(c_min, c_max, val):
     
@@ -38,9 +40,7 @@ def reassign_int(c_min, c_max, val):
         return val.astype(np.int64)
 
 def reassign_float(c_min, c_max, val):
-    if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
-        return val.astype(np.float16)
-    elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+    if c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
         return val.astype(np.float32)
     else:
         return val.astype(np.float64)    
@@ -94,4 +94,41 @@ def normalize(df):
     var=df.var()
     print(mean)
     print(var)
-    return (df-mean)/var
+    return (df-mean)/var, mean, var
+
+def calc_group_mean(df,group_of,mean_of):
+    return df.groupby(group_of)[mean_of].mean().to_dict()
+
+def map_to_groups(df,group_of,map_dict,convert_type):
+    return df[group_of].map(map_dict).astype(convert_type)
+
+def ignore_categories_nan(threshold,df,categories):
+    non_ignored_non_categories = []
+    ignored_non_categories = []
+    n_dataset = df.shape[0]
+    for elem in categories:
+        n_unknown = df[elem].isnull().sum()
+        ratio = float(n_unknown/n_dataset)
+        if ratio < threshold:
+            non_ignored_non_categories.append(elem)
+        else:
+            ignored_non_categories.append(elem)
+    return non_ignored_non_categories, ignored_non_categories
+
+class min_max:
+    def __init__(self, min, max):
+        self.min = min
+        self.max = max
+
+def min_max_scale(df, categories, min_max_dict = None):
+    override = False
+    if min_max_dict is None:
+        override = True
+        min_max_dict = {}
+    df_new = pd.DataFrame()
+    for elem in categories:
+        if override:
+            min_max_dict[elem] = min_max(min=df[elem].min(),max=df[elem].max())
+        df_new[elem] = df[elem]/(min_max_dict[elem].max - min_max_dict[elem].min)
+
+    return df_new,min_max_dict
